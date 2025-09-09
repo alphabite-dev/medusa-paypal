0bdb67840957298a88dafca8953c1ad1df917493
Ivan <ivan@alphabite.io>
2025-09-09 18:52:57 +0300

    update package version & options type export

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,10 @@
-import { type AlphabitePaypalPluginOptionsType } from "../providers/paypal/service";
+import { type AlphabitePaypalPluginOptions } from "../providers/paypal/service";
 import { type WebhookPayload } from "../providers/paypal/types";
 import { type PaypalCreateOrderInput } from "../providers/paypal/paypal-core/paypal-core";
 import { type PostStorePaypalPaymentType } from "../api/store/paypal/client-token/validators";
 
 export type {
-  AlphabitePaypalPluginOptionsType,
+  AlphabitePaypalPluginOptions,
   WebhookPayload,
   PaypalCreateOrderInput,
   PostStorePaypalPaymentType,
